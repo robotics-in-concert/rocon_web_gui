1aa92e9d2e2cdff5c64fe35ff52721959f78cddc
Jihoon Lee <jihoonlee.in@gmail.com>
2015-01-26 18:43:48 +0900

    add conductor viewer

---
+++ concert_conductor_graph_web/www/index.js
@@ -0,0 +1,104 @@
+var ros = new ROSLIB.Ros();
+var defaultUrL = rocon_interactions.rosbridge_uri;
+
+var viewer;
+
+//set sub
+var dotgraph_string_sub = '';
+var dotgraph_string_sub_topic_name = '/concert/conductor/graph_string';
+var dotgraph_string_sub_topic_type = 'std_msgs/String';
+
+var dotgraph_string = null;
+
+$().ready(function(e) {
+
+  initHeader();
+  initViewer();
+});
+
+function initHeader()
+{
+  settingROSCallbacks();
+  ros.connect(defaultUrL);
+  $('#focusedInput').val(''+defaultUrL);
+}
+
+function initViewer() {
+  viewer = createViewer();
+}
+
+function createViewer() {
+  
+  var view = getElementById('view');
+  view.innerHTML = "No Data received";
+}
+
+function settingROSCallbacks()                      
+{
+  ros.on('connection',function() {
+    console.log("Connected");
+    // subscribe to order list                                                       
+    $('#focusedInput').val('Connected');
+    $('#focusedInput').attr('disabled',true);
+    settingSubscriber();
+    }
+  );
+  ros.on('error',function(e) {
+    console.log("Error!",e);
+  }
+  );                                               
+  ros.on('close',function() {
+    console.log("Connection Close!");
+    $('#focusedInput').val('Disconnected');
+  }
+  );
+}
+
+function settingSubscriber()
+{
+  dotgraph_string_sub_topic = new ROSLIB.topic({
+    ros: ros,
+    name: dotgraph_string_sub_topic_name,
+    messageType: dotgraph_string_sub_topic_type
+  }); 
+  dotgraph_string_sub_topic.subscribe(processConductorGraph);
+}
+
+function processConductorGraph(msg)
+{
+  dotgraph_string = msg.data;
+  updateViewer();
+}
+
+function updateViewer()
+{
+  var view = getElementById('view');
+
+  if(dotgraph_string != null) {
+    drawDotGraph(view, dotgraph_string);
+  }
+}
+
+function drawDotGraph(view, data)
+{
+  var v = getViz(data);  
+  view.innerHTML += v;
+}
+
+function getViz(data)
+{
+  var result;
+  try {
+    result = Viz(data, format, engine);
+    if(format === "svg")
+      return result;
+    else
+      return inspect(result);
+  }catch(e) {
+    return inspect(e.toString());
+  }
+}
+
+function inspect(s) {
+  return "<pre>" + s.replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/\"/g, "&quot;") + "</pre>"
+}
