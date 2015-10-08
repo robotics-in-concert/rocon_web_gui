14a83476f81d44c6a4fa842b7b67ce91b8975b76
HyungRyul Kim <hrkim@yujinrobot.com>
2015-10-08 13:09:43 +0900

    first commit

---
+++ cafe_delivery_order/src/cafe_delivery_order/cafe_delivery_order_edge.js
@@ -0,0 +1,54 @@
+/*jslint */
+/*global AdobeEdge: false, window: false, document: false, console:false, alert: false */
+(function (compId) {
+
+    "use strict";
+    var im='images/',
+        aud='media/',
+        vid='media/',
+        js='js/',
+        fonts = {
+        },
+        opts = {
+            'gAudioPreloadPreference': 'auto',
+            'gVideoPreloadPreference': 'auto'
+        },
+        resources = [
+        ],
+        scripts = [
+        ],
+        symbols = {
+            "stage": {
+                version: "6.0.0",
+                minimumCompatibleVersion: "5.0.0",
+                build: "6.0.0.400",
+                scaleToFit: "both",
+                centerStage: "both",
+                resizeInstances: false,
+                content: {
+                    dom: [
+
+                    ],
+                    style: {
+                        '${Stage}': {
+                            isStage: true,
+                            rect: [undefined, undefined, '768px', '1024px'],
+                            overflow: 'hidden',
+                            fill: ["rgba(255,255,255,1)"]
+                        }
+                    }
+                },
+                timeline: {
+                    duration: 0,
+                    autoPlay: true,
+                    data: [
+
+                    ]
+                }
+            }
+        };
+
+    AdobeEdge.registerCompositionDefn(compId, symbols, fonts, scripts, resources, opts);
+
+    if (!window.edge_authoring_mode) AdobeEdge.getComposition(compId).load("cafe_delivery_order_edgeActions.js");
+})("EDGE-67918046");
